f6515e92736aea7cd710dacfe60c53777c700acb
Manu <59324352+manupythondeveloper@users.noreply.github.com>
2020-12-05 20:01:13 +0530

    Update Again functionality.
    line number 86: forgot to add data type.
    line number 88: instead of displayMessage querySelector error free.

--- 05-Guess-My-Number/final/script.js
+++ 05-Guess-My-Number/final/script.js
@@ -83,10 +83,10 @@ document.querySelector('.check').addEventListener('click', function () {
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  // document.querySelector('.message').textContent = 'Start guessing...';
-  displayMessage('Start guessing...');
+  document.querySelector('.message').textContent = 'Start guessing...';
+  // displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
